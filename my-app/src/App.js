8480d16dd41fce8e356638898f5da3f78b893c45
franky3020 <210238hwsh@gmail.com>
2023-09-02 23:10:33 +0800

    將拖動限制在一個區域

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,24 +6,29 @@ import hk from "./assets/cards/hk_compressed.jpg";
 function App() {
   return (
     <div className="App">
+
+      <div class="card-zoom">
+
       <Rnd
         default={{
-          x: 0,
-          y: 0,
-          width: 320,
-          height: 200,
+          x: 100,
+          y: 100,
         }}
+        bounds={'parent'}
       >
         <div>
           <img
             className="Cannot-select"
             src={hk}
+            width={80}
             alt="hk_compressed"
             draggable="false"
           />
         </div>
 
       </Rnd>
+
+      </div>
     </div>
   );
 }
