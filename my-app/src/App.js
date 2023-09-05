b5ed246163cfc529da218607968eb6d901f23a30
franky3020 <210238hwsh@gmail.com>
2023-09-05 23:24:25 +0800

    解決拖動bug

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,33 @@
-import './App.css';
+import "./App.css";
 import { Rnd } from "react-rnd";
-import { useState } from 'react';
+import { useState } from "react";
 
 import hkCard from "./assets/cards/hk_compressed.jpg";
 
 import cqCard from "./assets/cards/cq_compressed.jpg";
 
-
 function App() {
-
   const [showCardSrc, setShowCardSrc] = useState(hkCard);
+
+  const [cardx, setCardx] = useState(100);
+  const [cardy, setCardy] = useState(200);
 
   function hideCard(e) {
     if (showCardSrc === hkCard) {
       // TODO: 需修正成卡片背面
       setShowCardSrc(cqCard);
     }
-    
   }
 
   function onDragStart(e, data) {
     hideCard();
+    // setInterval(() => {
+    //   setCardx(cardx => cardx + 20);
+    //   setCardy(cardy => cardy + 20);
+    // }, 500);
+
+    // position={{ x: cardx, y: cardy }}
   }
-
-
 
   return (
     <div className="App">
@@ -32,69 +36,12 @@ function App() {
           x: 100,
           y: 100,
         }}
-        bounds={'parent'}
+        bounds={"parent"}
         onDragStart={onDragStart}
+        enableResizing={false}
       >
         <div onClick={hideCard}>
           <img
-            className="Cannot-select"
-            src={showCardSrc}
-            width={80}
-            alt="hk_compressed"
-            draggable="false"
-          />
-        </div>
-      </Rnd>
-
-      <Rnd
-        default={{
-          x: 200,
-          y: 200,
-        }}
-        bounds={'parent'}
-        onDragStart={onDragStart}
-      >
-        <div onClick={hideCard}>
-          <img
-            className="Cannot-select"
-            src={showCardSrc}
-            width={80}
-            alt="hk_compressed"
-            draggable="false"
-          />
-        </div>
-      </Rnd>
-
-      <Rnd
-        default={{
-          x: 100,
-          y: 300,
-        }}
-        bounds={'parent'}
-        onDragStart={onDragStart}
-      >
-        <div onClick={hideCard}>
-          <img
-            className="Cannot-select"
-            src={showCardSrc}
-            width={80}
-            alt="hk_compressed"
-            draggable="false"
-          />
-        </div>
-      </Rnd>
-
-      <Rnd
-        default={{
-          x: 200,
-          y: 400,
-        }}
-        bounds={'parent'}
-        onDragStart={onDragStart}
-      >
-        <div onClick={hideCard}>
-          <img
-            className="Cannot-select"
             src={showCardSrc}
             width={80}
             alt="hk_compressed"
