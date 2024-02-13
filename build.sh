e71e818c7a97b8cdfa6d46781ba726d655b75843
franky3020 <210238hwsh@gmail.com>
2024-02-13 21:28:54 +0800

    feat: add ios platform

--- build.sh
+++ build.sh
@@ -6,7 +6,11 @@ npm run build
 cd ../cordova-app
 rm -rf www
 cp -r ../web/build ./www
+touch ./www/.gitkeeper
 npm i
 npx cordova prepare
 npx cordova platform rm android --nosave
 npx cordova platform add android
+
+npx cordova platform rm ios --nosave
+npx cordova platform add ios
