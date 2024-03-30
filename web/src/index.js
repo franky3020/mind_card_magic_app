d22215ae3de64b732ff47b12d137940094a836b7
franky3020 <210238hwsh@gmail.com>
2024-03-30 20:54:48 +0800

    設定 ios 發布id

--- web/src/index.js
+++ web/src/index.js
@@ -1,43 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {HashRouter, Route, Routes} from "react-router-dom";
-import {MindMagicPage} from "./pages/MindMagicPage";
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { MindMagicPage } from "./pages/MindMagicPage";
 
 const renderReactDom = () => {
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/magicPage" element={<MindMagicPage />} />
+      </Routes>
+    </HashRouter>
+  );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <HashRouter>
-    <Routes>
-      <Route path="/" element={ <App /> } />
-      <Route path="/magicPage" element={ <MindMagicPage /> } />
-    </Routes>
-  </HashRouter>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
 };
 
-
-
 if (window.cordova) {
-  document.addEventListener('deviceready', () => {
-    renderReactDom();
-  }, false);
+  document.addEventListener(
+    "deviceready",
+    () => {
+      renderReactDom();
+    },
+    false
+  );
 } else {
   renderReactDom();
 }
-
-
-
-
-
-
