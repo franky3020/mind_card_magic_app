af251ebd7d6fcb248f15b9996d2ee053a0b280b4
franky3020 <210238hwsh@gmail.com>
2024-04-17 23:30:27 +0800

    加入 logo

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,28 @@
 import "./App.css";
 import {Link} from "react-router-dom";
+import appLogo from "./assets/app_logo.png";
+
 
 export default function App() {
 
   return (
     <div className="App">
-      <Link to="/magicPage">
-        <button className="btn waves-effect waves-light">Start</button>
-      </Link>
+      <div className="TAIL-flex TAIL-justify-center TAIL-my-8">
+        <img src={appLogo} alt='appLogo' className="TAIL-w-10/12"/>
+      </div>
+
+      <div className="TAIL-flex TAIL-justify-center TAIL-my-8">
+        <Link to="/magicPage">
+          <button className="btn grey lighten-5 waves-effect waves-light btn-large pulse">Start</button>
+        </Link>
+      </div>
+
+      <div className="TAIL-flex TAIL-justify-center TAIL-my-8">
+        <Link to="/magicPage">
+          <button className="btn waves-effect waves-light btn-large">Tutorial</button>
+        </Link>
+      </div>
+
     </div>
   );
 }
